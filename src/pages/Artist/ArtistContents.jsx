817500f9500585268a1d2872248bf9232e57ddf0
wemwos <martmodesto2021@gmail.com>
2024-05-10 03:47:17 +0800

    ArtistContentPagination

--- src/pages/Artist/ArtistContents.jsx
+++ src/pages/Artist/ArtistContents.jsx
@@ -5,36 +5,53 @@ import ArtistLayout from '../../Layout/ArtistLayout';
 import { Image } from 'primereact/image';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, IconButton, Menu, MenuItem, TextField } from '@mui/material';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import defaultCover from '../../assets/img/default.png'
+import defaultCover from '../../assets/img/default.png';
+import Pagination from '@mui/material/Pagination'; // Import Pagination component
 
 function ArtistContents() {
+    // State variables
     const [selectedTab, setSelectedTab] = useState(0);
     const [musicData, setMusicData] = useState([
         { id: 1, albumCover: defaultCover, title: 'Song 1', dateUploaded: '2024-04-13', totalListens: 100, status: 'Posted' },
-        { id: 2, albumCover: defaultCover, title: 'yawa', dateUploaded: '2024-04-12', totalListens: 200, status: 'Draft' },
-        { id: 3, albumCover: defaultCover, title: 'sili', dateUploaded: '2024-04-11', totalListens: 550, status: 'Posted' },
+        { id: 2, albumCover: defaultCover, title: 'Song 2', dateUploaded: '2024-04-12', totalListens: 200, status: 'Draft' },
+        { id: 3, albumCover: defaultCover, title: 'move', dateUploaded: '2024-04-11', totalListens: 550, status: 'Posted' },
         { id: 4, albumCover: defaultCover, title: 'gague', dateUploaded: '2024-04-11', totalListens: 450, status: 'Posted' },
-        { id: 5, albumCover: defaultCover, title: 'Song 5', dateUploaded: '2024-04-11', totalListens: 150, status: 'Removed' },
-        { id: 6, albumCover: defaultCover, title: 'Song 6', dateUploaded: '2024-04-11', totalListens: 850, status: 'Removed' },
+        { id: 5, albumCover: defaultCover, title: 'Song 5', dateUploaded: '2024-04-11', totalListens: 150, status: 'Posted' },
+        { id: 6, albumCover: defaultCover, title: 'Song 6', dateUploaded: '2024-04-11', totalListens: 850, status: 'Posted' },
+        { id: 7, albumCover: defaultCover, title: 'deadline', dateUploaded: '2024-04-11', totalListens: 850, status: 'Removed' },
+        { id: 8, albumCover: defaultCover, title: 'please', dateUploaded: '2024-04-11', totalListens: 850, status: 'Posted' },
+        { id: 9, albumCover: defaultCover, title: 'Song 6', dateUploaded: '2024-04-11', totalListens: 850, status: 'Posted' },
+
     ]);
     const [anchorEl, setAnchorEl] = useState(null);
-    const [deleteId, setDeleteId] = useState(null); // Store the ID of the item to be deleted
-    const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' }); // State for sorting configuration
+    const [deleteId, setDeleteId] = useState(null);
+    const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
     const [searchQuery, setSearchQuery] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [rowsPerPage] = useState(5); // Number of items per page
 
-    const handleClick = (event, id) => { // Pass the id as argument
+    // Pagination handler
+    const handlePageChange = (event, value) => {
+        setCurrentPage(value);
+    };
+
+    // Calculate indexes for pagination
+    const indexOfLastItem = currentPage * rowsPerPage;
+    const indexOfFirstItem = indexOfLastItem - rowsPerPage;
+    const currentData = musicData.slice(indexOfFirstItem, indexOfLastItem);
+
+    const handleClick = (event, id) => {
         setAnchorEl(event.currentTarget);
-        setDeleteId(id); // Set the ID of the item to be deleted
+        setDeleteId(id);
     };
 
     const handleClose = () => {
         setAnchorEl(null);
-        setDeleteId(null); // Reset the deleteId state
+        setDeleteId(null);
     };
 
-    const handleDelete = (id) => { // Only 
+    const handleDelete = (id) => {
         setMusicData(prevMusicData => {
-            // Filter based on the id and update the IDs of the remaining rows
             const updatedData = prevMusicData.filter(item => item.id !== id).map((item, index) => ({ ...item, id: index + 1 }));
             return updatedData;
         });
@@ -73,15 +90,15 @@ function ArtistContents() {
     };
 
     const handleSearchInputChange = (event) => {
-        const value = event.target.value.toLowerCase(); // Convert search query to lowercase
-        setSearchQuery(value); // Update search query state
+        const value = event.target.value.toLowerCase();
+        setSearchQuery(value);
     };
 
     const filteredData = musicData.filter(item =>
         item.title.toLowerCase().includes(searchQuery)
     );
 
-    const renderTableRows = (data) => { // Remove dataType parameter since it's specific to musicData
+    const renderTableRows = (data) => {
         return data.map((row, index) => (
             <TableRow key={row.id} style={{ backgroundColor: index % 2 === 0 ? '#f2f2f2' : 'white' }}>
                 <TableCell onClick={() => requestSort('id')} className={getClassNamesFor('id')}>
@@ -89,7 +106,7 @@ function ArtistContents() {
                         #{row.id}
                     </div>
                 </TableCell>
-                <TableCell><img src={row.albumCover} alt="Album Cover" style={{ width: '80px', height: 'auto' }} /></TableCell>
+                <TableCell><img src={row.albumCover} alt="Album Cover" style={{ width: '50px', height: 'auto' }} /></TableCell>
                 <TableCell onClick={() => requestSort('title')} className={getClassNamesFor('title')}>
                     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100px' }}>
                         {row.title}
@@ -107,8 +124,8 @@ function ArtistContents() {
                 </TableCell>
                 <TableCell>{row.status}</TableCell>
                 <TableCell>
-                    <IconButton onClick={(event) => handleClick(event, row.id)}> {/* Pass the id here */}
-                        <MoreVertIcon style={{ fontSize: '20px' }} /> 
+                    <IconButton onClick={(event) => handleClick(event, row.id)}>
+                        <MoreVertIcon style={{ fontSize: '20px' }} />
                     </IconButton>
                     <Menu
                         anchorEl={anchorEl}
@@ -123,7 +140,7 @@ function ArtistContents() {
                             horizontal: 'right',
                         }}
                         getContentAnchorEl={null}
-                        MenuProps={{ 
+                        MenuProps={{
                             PaperProps: {
                                 style: {
                                     backgroundColor: '#ffffff',
@@ -131,7 +148,7 @@ function ArtistContents() {
                             },
                         }}
                     >
-                        <MenuItem onClick={() => handleDelete(deleteId)} style={{ backgroundColor: '#ffffff' }}>Delete</MenuItem> {/* Use deleteId state here */}
+                        <MenuItem onClick={() => handleDelete(deleteId)} style={{ backgroundColor: '#ffffff' }}>Delete</MenuItem>
                         <MenuItem style={{ backgroundColor: '#ffffff' }}>View</MenuItem>
                     </Menu>
                 </TableCell>
@@ -167,22 +184,22 @@ function ArtistContents() {
                         style: { color: 'white' }
                     }}
                 />
-
+    
                 <Tabs selectedIndex={selectedTab} onSelect={index => setSelectedTab(index)} style={{ width: '100%', backgroundColor: 'black' }}>
                     <TabList style={{ width: 'fit-content', backgroundColor: 'black' }}>
                         <Tab style={{ width: '100px', marginRight: '10px', backgroundColor: selectedTab === 0 ? '#EEE3E1' : 'transparent', color: selectedTab === 0 ? 'black' : 'gray' }}>Music</Tab>
                         <Tab style={{ width: '100px', marginRight: '10px', backgroundColor: selectedTab === 1 ? '#EEE3E1' : 'transparent', color: selectedTab === 1 ? 'black' : 'gray' }}>Podcast</Tab>
                         <Tab style={{ width: '100px', marginRight: '10px', backgroundColor: selectedTab === 2 ? '#EEE3E1' : 'transparent', color: selectedTab === 2 ? 'black' : 'gray' }}>Videocast</Tab>
                     </TabList>
-
+    
                     <TabPanel>
                         {filteredData.length === 0 ? (
                             <div className="no-records text-white">There are no records to display for Music</div>
                         ) : (
                             <div className="scrollable-table">
-                                <TableContainer component={Paper} style={{ maxHeight: '400px', overflowY: 'auto' }}>
-                                    <Table>
-                                        <TableHead style={{ position: 'sticky', top: 0, backgroundColor: 'white', zIndex: 1 }}>
+                                <TableContainer component={Paper}>
+                                    <Table size="small">
+                                        <TableHead>
                                             <TableRow>
                                                 <TableCell onClick={() => requestSort('id')} className={getClassNamesFor('id')}>
                                                     # {sortConfig.key === 'id' && (sortConfig.direction === 'ascending' ? '\u25B2' : '\u25BC')}
@@ -202,20 +219,26 @@ function ArtistContents() {
                                             </TableRow>
                                         </TableHead>
                                         <TableBody>
-                                            {renderTableRows(sortedData())} {/* Use filteredData here */}
+                                            {renderTableRows(currentData)}
                                         </TableBody>
                                     </Table>
                                 </TableContainer>
                             </div>
                         )}
                     </TabPanel>
-                    <TabPanel>
-                        {/* Similar structure as above */}
-                    </TabPanel>
-                    <TabPanel>
-                        {/* Similar structure as above */}
-                    </TabPanel>
+                    {/* Add similar TabPanel components for Podcast and Videocast */}
                 </Tabs>
+                {/* Pagination */}
+                <div style={{ position: 'fixed', bottom: '20px', left: '750px', transform: 'translateX(-50%)' }}>
+    <Pagination
+        count={Math.ceil(filteredData.length / rowsPerPage)}
+        page={currentPage}
+        onChange={handlePageChange}
+        variant="outlined"
+        color="primary"
+        style={{ backgroundColor: 'white' }} // Change the background color here
+    />
+</div>
             </div>
         </ArtistLayout>
     );
